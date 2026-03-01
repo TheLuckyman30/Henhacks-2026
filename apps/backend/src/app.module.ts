67bf4cbdfa6f2a7ed4fefc7ecc7068805b1e8488
TheLuckyman30 <bnauta0@gmail.com>
2026-02-28 19:04:32 -0500

    Create posting endpoint working

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma.service';
+import { PostingModule } from './posting/posting.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController, UserController],
+  imports: [UserModule, PostingModule],
+  controllers: [AppController],
   providers: [AppService, UserService, PrismaService],
 })
 export class AppModule {}
--- apps/backend/src/prisma.service.ts
+++ apps/backend/src/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@repo/database';
 import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
